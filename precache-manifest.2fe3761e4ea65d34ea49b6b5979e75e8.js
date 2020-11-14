cb6dcaaf6814951c79fd6f4baf9190f1cb471810
hamzatech2424 <hamzatech2424@gmail.com>
2020-11-15 01:28:28 +0500

    Updates

---
+++ precache-manifest.2fe3761e4ea65d34ea49b6b5979e75e8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2bea37bb6ed157200fd04c0de39c62",
+    "url": "/olxapp/index.html"
+  },
+  {
+    "revision": "dd79e298f3b8ad5350e6",
+    "url": "/olxapp/static/css/main.3fbf37b3.chunk.css"
+  },
+  {
+    "revision": "611430994e574bdc56e8",
+    "url": "/olxapp/static/js/2.806f4b76.chunk.js"
+  },
+  {
+    "revision": "cecd015c6b6df2870bde130f0e87ceda",
+    "url": "/olxapp/static/js/2.806f4b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd79e298f3b8ad5350e6",
+    "url": "/olxapp/static/js/main.ea4931cb.chunk.js"
+  },
+  {
+    "revision": "b62031f8a33741c73fc0",
+    "url": "/olxapp/static/js/runtime-main.2b27b3e6.js"
+  },
+  {
+    "revision": "c9531080944dd02d3d2146b708c3bffa",
+    "url": "/olxapp/static/media/avatar.c9531080.png"
+  },
+  {
+    "revision": "161a91af07f0226cff8be365653ae7d1",
+    "url": "/olxapp/static/media/banner1.161a91af.jpg"
+  },
+  {
+    "revision": "ba7da51b0a098ae08b29480c2cc21a4c",
+    "url": "/olxapp/static/media/note9.ba7da51b.jpg"
+  },
+  {
+    "revision": "2246718ee750e4abdda5516ad01c8e0a",
+    "url": "/olxapp/static/media/staticmap.2246718e.png"
+  }
+]);
